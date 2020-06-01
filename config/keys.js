013b5c4d52ad1092fc9052c6efb3426864c66457
Lee Ann Norman <leeann.norm@gmail.com>
2020-06-01 06:44:01 -0500

    all working! final formatting being added...

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,4 @@ if(process.env.NODE_ENV==='production'){
 }else{
     module.exports = require('./dev')
 }
-
 // Atlas API uses HTTP Digest Authentication. It essentially requires a username and a password which are hashed using a unique server-generated value called a nonce. The username is the API public key and the password is the corresponding private key.
