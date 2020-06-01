5d7ad4dc055e9dad091864a3d7b97daa1db9905a
Lee Ann Norman <leeann.norm@gmail.com>
2020-06-01 05:20:40 -0500

    adding additional notes

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -23,3 +23,7 @@ module.exports = (req, res, next) => {
         });
     });
 }
+
+// REDUX: Actions are payloads of information that send data from your application to your store. They are the only source of information for the store. You send them to the store using store.dispatch().
+
+// Actions are plain JavaScript objects. Actions must have a type property that indicates the type of action being performed. Types should typically be defined as string constants. Once your app is large enough, you may want to move them into a separate module.
