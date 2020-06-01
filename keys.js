96404f306f4dd31895c51902e50b0e3405f97111
Lee Ann Norman <leeann.norm@gmail.com>
2020-06-01 04:26:57 -0500

    still not running backend

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGOURI: "mongodb+srv://sai_01:GY7s5M3oE63zxPJK@cluster0-g2t9m.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "5TNFrP76vRQ7Vd7sJRcjqDkmrTsFckXPKkBuQga6"
+}
