6cbc3e982a6987405eb4ebc6161286c98502420e
Lee Ann Norman <leeann.norm@gmail.com>
2020-06-01 08:49:16 -0500

    edited app.js file

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
-const express = require('express')
-const app = express()
-const mongoose  = require('mongoose')
-const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const express = require('express');
+const app = express();
 
+// Mongo DB
+const mongoose = require('mongoose');
+const {MONGOURI} = require('./keys');
 
-mongoose.connect(MONGOURI,{
+mongoose.connect(MONGOURI, {
     useNewUrlParser:true,
     useUnifiedTopology: true
+});
+mongoose.connection.on('connected', ()=>{
+    console.log('Database connected');
+});
+mongoose.connection.on('error', (error)=>{
+    console.log('Database NOT connected', error);
+});
 
-})
-mongoose.connection.on('connected',()=>{
-    console.log("conneted to mongo yeahh")
-})
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting",err)
-})
+// Models to add schema
+require('./models/user');
+require('./models/post');
 
-require('./models/user')
-require('./models/post')
+// Routes to add middleware
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
 
-app.use(express.json())
-app.use(require('./routes/auth'))
-app.use(require('./routes/post'))
-app.use(require('./routes/user'))
-
-
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
-
+//PORT to run server
 app.listen(process.env.PORT || 5000,()=>{
     console.log("server is running on",PORT)
 })
