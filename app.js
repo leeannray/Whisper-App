c4fde82c9319e73520d71f190caf6e86074a6ace
Lee Ann Norman <leeann.norm@gmail.com>
2020-06-01 04:18:14 -0500

    fixed issues with running backend server

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-//mongoose for mongo db database
+const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-// local port 5000
 const {MONGOURI} = require('./config/keys')
 
 
--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -21,6 +21,7 @@ module.exports = (req,res,next)=>{
         User.findById(_id).then(userdata=>{
             req.user = userdata
             next();
+            // next will go to next middleware
         });        
     });
 }
